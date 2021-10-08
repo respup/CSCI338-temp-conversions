14b29b52e90e4ed2f40b4b4ad11a64927348681e
lodowd21 <60274768+lodowd21@users.noreply.github.com>
2021-10-07 22:02:39 -0400

    Update Fahrenheit.java
    added a celsius frame

--- src/Fahrenheit.java
+++ src/Fahrenheit.java
@@ -21,5 +21,16 @@ public class Fahrenheit
 
 		frame.pack();
 		frame.setVisible(true);
+		
+		
+        	JFrame cFrame = new JFrame("Celsius");
+		cFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		CelsiusPanel cPanel = new CelsiusPanel();
+		cFrame.getContentPane().add(cPanel);
+
+		cFrame.pack();
+		cFrame.setVisible(true);
+		
 	}
 }
